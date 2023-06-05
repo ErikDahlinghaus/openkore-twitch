e87cd71011eda3b9e84efee956fbb424f826d59e
Erik Dahlinghaus <erik.dahlinghaus@gmail.com>
2023-06-04 20:35:32 -0700

    Prevents people from creating aliases with eval inside

--- twitch/twitch.pl
+++ twitch/twitch.pl
@@ -177,6 +177,12 @@ sub iterate {
             next;
          }
 
+         if( $userCommand =~ /^conf alias/ ) {
+            my $responseMessage = "PRIVMSG " . $channel . " :BOT BLOCKED @" . $user . " from running command \"" . $userCommand . "\"\r\n";
+            print $socket $responseMessage;
+            next;
+         }
+
          my $cmd = lc $params[0];
          my @blocked = grep { lc $_ eq $cmd } @blockedCommands;
          if( scalar @blocked != 0 ) {
